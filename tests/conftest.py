3f0c097806107716cd9b2082dc358ace134eb15f
Manuel <manurz2121@gmail.com>
2024-09-15 20:47:08 -0300

    Make tests truly independent

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,20 @@ from app.config import TestConfig
 
 
 @pytest.fixture(scope="module")
-def test_client():
+def app():
     app = create_app(TestConfig)
-    with app.test_client() as testing_client:
-        with app.app_context():
-            db.create_all()
-            yield testing_client
-            db.drop_all()
+    with app.app_context():
+        yield app
+
+
+@pytest.fixture(scope="module")
+def test_client(app):
+    with app.test_client() as client:
+        yield client
+
+
+@pytest.fixture(scope="function")
+def setup_database():
+    db.create_all()
+    yield
+    db.drop_all()
